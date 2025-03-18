6238ff51579871fa2b7ef2ffdfed48397abc8265
satwik-naik <naiksatwik7@gmail.com>
2025-03-18 15:50:15 +0530

    Describe this commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "Hello, World PES1UG23CS825\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this again x1 \n";
   return 0;
