0586ca4e8653a92c1adf0de34ca81e76f6528458
satwik-naik <naiksatwik7@gmail.com>
2025-03-20 23:54:06 +0530

    2nd commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World PES1UG23CS825\n";
-  cout << "Hello, Jenkins\n";
+  cout << "Hello, Satwik naik\n";
   cout << "I have successfully built and run this again x1 \n";
   return 0;
 }
